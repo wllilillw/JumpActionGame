42e5e62c31f4dab63996e9227bb2618193256139
ユーザー名 <kanokuda.1@gmail.com>
2019-09-13 18:20:51 +0900

    Initial commit

---
+++ android/src/jp/techacademy/okuda/kanji/jumpacactiongame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package jp.techacademy.okuda.kanji.jumpacactiongame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import jp.techacademy.okuda.kanji.jumpacactiongame.JumpActionGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new JumpActionGame(), config);
+	}
+}
